5c27a196e07514d5b441231bf01a7b1a589293b2
Alina Trifonova <trifonova.alina@mail.ru>
2022-08-11 11:44:16 +1000

    init

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 9_000;
+        int amountRub = 20;
+        System.out.println(ticketPrice / amountRub);
+    }
+}
